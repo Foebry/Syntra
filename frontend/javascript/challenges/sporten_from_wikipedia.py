cc7e9654d712c33fe696a28529005c292d7d2a2f
Sander Fabry <rain_fabry@hotmail.com>
2022-01-18 17:26:47 +0100

    improved sporten_from_wikipedia script

--- frontend/javascript/challenges/sporten_from_wikipedia.py
+++ frontend/javascript/challenges/sporten_from_wikipedia.py
@@ -2,26 +2,48 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
+olympische_sporten = []
+sporten_dict = {}
+
 page = requests.get("https://nl.wikipedia.org/wiki/Lijst_van_sporten")
-# print(vars(data).keys())
 soup = BeautifulSoup(page.content, "html.parser")
-content = soup.find(class_="mw-parser-output")
-titles = content.findAll("h2")
+
+content = soup.find("div", {"class": "mw-parser-output"})
 dls = content.findAll("dl")
-
-# print(dls[1].findAll("dd")[0].findAll("a")[i].text)
-
-sporten = {titles[i].text.split("[")[0].lower(): "" for i in range(1, len(titles))}
+titles = [title.text.split("[")[0].lower().replace("\u00eb", "e") for title in content.findAll("h2")[1:]]
 
 
-for i in range(len(list(sporten.keys()))):
-    sport = list(sporten.keys())[i]
-    sporten[sport] = [
-        dls[i].findAll("dd")[0].findAll("a")[j].text
-        # for i in range(len(dls))
-        for j in range(len(dls[i].findAll("dd")[0].findAll("a")))
-    ]
+j = 0
+for i in range(len(titles)):
+    title = titles[i]
+    sporten = []
+
+    times = 4 if i == 1 else 1
+    for _ in range(times):
+        section = dls[j].findAll("dd")
+        # quit(print(namen))
+        namen = section[0].findAll("a")
+        for naam in namen:
+            sporten.append(naam.text.lower().strip().replace("\u00eb", "e"))
+        j += 1
+
+    sporten_dict[title] = sporten
+
+
+page = requests.get("https://nl.m.wikipedia.org/wiki/Olympische_sport")
+soup = BeautifulSoup(page.content, "html.parser")
+content = soup.find("table", class_="wikitable")
+content_rows = content.findAll("tr")
+
+for row in content_rows[1:]:
+    column = row.findAll("td")[-1]
+    for link in column.findAll("a"):
+        link_items = link.text.split(",")
+        for sport in link_items:
+            olympische_sporten.append(sport.split("(")[0].strip().lower())
+
+sporten_dict["olympische_sporten"] = olympische_sporten
 
 
 with open("./sporten.json", "w") as file:
-    json.dump(sporten, file)
+    json.dump(sporten_dict, file, indent=4)
