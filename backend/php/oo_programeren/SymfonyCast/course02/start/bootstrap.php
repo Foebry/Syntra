e08d81a3356f2215a4bf65497f51fce61eb0e904
Sander Fabry <rain_fabry@hotmail.com>
2022-02-19 12:13:06 +0100

    chapter 13

--- backend/php/oo_programeren/SymfonyCast/course02/start/bootstrap.php
+++ backend/php/oo_programeren/SymfonyCast/course02/start/bootstrap.php
@@ -1,9 +1,10 @@
 <?php
 
-require_once "./models/Ship.php";
-require_once "./models/ShipLoader.php";
-require_once "./models/BattleResult.php";
-require_once "./models/Container.php";
+require_once "./lib/Model/Ship.php";
+require_once "./lib/Service/BattleManager.php";
+require_once "./lib/Service/ShipLoader.php";
+require_once "./lib/Model/BattleResult.php";
+require_once "./lib/Service/Container.php";
 
 
 $configuration = array(
